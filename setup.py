d15125ecef2c8b0279ecbe91574130237a9164ee
90houlaoheshang <907333918@qq.com>
2021-08-17 17:01:21 +0800

    更新Python版本要求

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="AsyncWebsocketStreamInterface",
-    version="0.9.6",
+    version="0.9.7",
     author="Antas",
     author_email="",
     description="Exchange one normal asynchronous websocket connection to unlimited number of data streams.",
@@ -18,5 +18,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.7',
 )
