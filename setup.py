e4fdc3669556ba67ae1dab5f207b29e1404392b1
90houlaoheshang <907333918@qq.com>
2021-08-20 16:19:47 +0800

    更新版本号

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="AsyncWebsocketStreamInterface",
-    version="0.9.9",
+    version="0.9.10",
     author="Antas",
     author_email="",
     description="Exchange one normal asynchronous websocket connection to unlimited number of data streams.",
